97fc1f7e163bb0eb45700cf7905ee47208cae694
erwinwahyura <erwinwahyuramadhan@gmail.com>
2017-12-13 14:59:57 +0700

    edit routes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const c_karyawan = require('../controllers/c_karyawan')
+// const c_karyawan = require('../controllers/c_karyawan')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/karyawan', c_karyawan.create);
-router.get('/karyawan', c_karyawan.getAllKaryawan);
-router.delete('/karyawan/:id', c_karyawan.deleteKaryawan);
-router.get('/karyawan/:id', c_karyawan.getKaryawanById);
+// router.post('/karyawan', c_karyawan.create);
+// router.get('/karyawan', c_karyawan.getAllKaryawan);
+// router.delete('/karyawan/:id', c_karyawan.deleteKaryawan);
+// router.get('/karyawan/:id', c_karyawan.getKaryawanById);
 
 module.exports = router;
