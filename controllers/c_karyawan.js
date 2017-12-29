e1104799bd0d4581400c74d32f24f78680305dbd
erwinwahyura <erwinwahyuramadhan@gmail.com>
2017-12-29 23:31:04 +0700

    delete column gol, pangkat

--- controllers/c_karyawan.js
+++ controllers/c_karyawan.js
@@ -5,9 +5,7 @@ module.exports = {
         db.karyawan.create({
             nip: req.body.nip,
             nama: req.body.nama,
-            jabatan: req.body.jabatan,
-            pangkat: req.body.pangkat,
-            gol: req.body.gol,
+            jabatan: req.body.jabatan
         })
         .then((response) => {res.status(200).send(response)})
         .catch((err) => {res.status(400).send(err)})
@@ -28,8 +26,6 @@ module.exports = {
                 nip: req.body.nip === null ? response.nip : req.body.nip,
                 nama: req.body.nama === null ? response.nama : req.body.nama,
                 jabatan: req.body.jabatan === null ? response.jabatan : req.body.jabatan,
-                pangkat: req.body.pangkat === null ? response.pangkat : req.body.pangkat,
-                gol: req.body.gol === null ? response.gol : req.body.gol,
                 updatedAt: new Date()
             }, {
                 where: {
