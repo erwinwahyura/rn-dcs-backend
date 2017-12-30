ccd039a652d208f369890fdd1e6a7a011316dd6e
erwinwahyura <erwinwahyuramadhan@gmail.com>
2017-12-30 19:52:54 +0700

    adding seeder absen, penilaians

---
+++ seeders/20171229153849-absen.js
@@ -0,0 +1,277 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('absens', [{
+      id_karyawan: 1,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 2,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 3,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 4,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 5,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 6,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 7,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 8,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 9,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 10,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 11,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 12,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 13,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 14,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 15,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 16,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 17,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 18,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 19,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 20,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 21,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 22,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 23,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 24,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 25,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 26,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 27,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 28,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 29,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 30,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 31,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 32,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 33,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 34,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 35,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 36,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 37,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 38,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 39,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 40,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 41,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 42,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 43,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 44,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 45,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 46,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 47,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 48,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 49,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id_karyawan: 50,
+      tgl: Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
