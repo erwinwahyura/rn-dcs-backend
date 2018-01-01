d18e05b2b2311f4d5b48ee78b32ad060c22d17b2
erwinwahyura <erwinwahyuramadhan@gmail.com>
2018-01-02 04:28:18 +0700

    add new routes for bulk insert

--- routes/process.js
+++ routes/process.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 var c_auth = require('../controllers/c_auth')
 var c_proses = require('../controllers/c_proses')
-
+var c_bulk = require('../controllers/c_bulk')
 /* GET users listing. */
 
 router.post('/', c_proses.getDataByWeek);
 router.post('/fuzzy', c_proses.fuzzyNR);
+router.post('/save', c_bulk._bulkInsert)
 
 module.exports = router;
