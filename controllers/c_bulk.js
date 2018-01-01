f06a511f8dcf5e4c18712f61418644fb148ceefc
erwinwahyura <erwinwahyuramadhan@gmail.com>
2018-01-02 04:28:08 +0700

    add new bulk insert

---
+++ controllers/c_bulk.js
@@ -0,0 +1,14 @@
+const db = require('../models');
+
+module.exports = {
+    _bulkInsert = (req, res) => {
+        const data = req.body.datas
+        db.penilaians.bulkCreate(data)
+        .then((response) => {
+            res.status(200).send(response)
+        })
+        .catch((err) => {
+            res.status(500).send(err)
+        })
+    }
+}
