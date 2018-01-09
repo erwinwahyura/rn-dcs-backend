07b87264be23e9b450b53100c748392ad2f73f5c
erwinwahyura <erwinwahyuramadhan@gmail.com>
2018-01-09 14:40:46 +0700

    fix search on report screen

--- controllers/c_nilai.js
+++ controllers/c_nilai.js
@@ -5,9 +5,11 @@ module.exports = {
     _allGetNilai: (req, res) => {
         db.nilai.sequelize.query(`
         select nilais.id, nilais.id_absen as "id_absen", nilais.nilai, nilais.tag, nilais.keterangan,
-        karyawans.id as "id_karyawan", karyawans.nip, karyawans.nama
-        from karyawans
-        left join nilais on karyawans.id = nilais.id_absen
+        karyawans.id as "id_karyawan", karyawans.nip, karyawans.nama,
+        absens.id as "id_absen"
+        from absens
+        inner join nilais on absens.id = nilais.id_absen
+        inner join karyawans on absens.id_karyawan = karyawans.id
         `, { type: db.nilai.sequelize.QueryTypes.SELECT })
         .then((response) => {
             res.status(200).send(response)
@@ -19,9 +21,11 @@ module.exports = {
     _getNilaiByWeek: (req, res) => {
         db.nilai.sequelize.query(`
         select nilais.id, nilais.id_absen as "id_absen", nilais.nilai, nilais.tag, nilais.keterangan,
-        karyawans.id as "id_karyawan", karyawans.nip, karyawans.nama
-        from karyawans
-        left join nilais on karyawans.id = nilais.id_absen
+        karyawans.id as "id_karyawan", karyawans.nip, karyawans.nama,
+        absens.id as "id_absen"
+        from absens
+        inner join nilais on absens.id = nilais.id_absen
+        inner join karyawans on absens.id_karyawan = karyawans.id
         where nilais.tag = '${req.body.week}'
         `, { type: db.nilai.sequelize.QueryTypes.SELECT })
         .then((response) => {
@@ -34,9 +38,11 @@ module.exports = {
     _getNilaiByName: (req, res) => {
         db.nilai.sequelize.query(`
         select nilais.id, nilais.id_absen as "id_absen", nilais.nilai, nilais.tag, nilais.keterangan,
-        karyawans.id as "id_karyawan", karyawans.nip, karyawans.nama
-        from karyawans
-        left join nilais on karyawans.id = nilais.id_absen
+        karyawans.id as "id_karyawan", karyawans.nip, karyawans.nama,
+        absens.id as "id_absen"
+        from absens
+        inner join nilais on absens.id = nilais.id_absen
+        inner join karyawans on absens.id_karyawan = karyawans.id
         where karyawans.nama = '${req.body.nama}'
         `, { type: db.nilai.sequelize.QueryTypes.SELECT })
         .then((response) => {
